fd47276a6d9e76808cf719a7b7cc08fc740c4bd6
adithyansyam <adithyansyam05@gmail.com>
2026-01-03 21:18:23 +0530

    added the map

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 import 'login_screen.dart';
 import 'profile_screen.dart';
 import '../widgets/homepage_navbar.dart';
@@ -22,25 +24,40 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget _buildHomeContent() {
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.check_circle_outline,
-              size: 80,
-              color: const Color(0xFF9C27B0),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              'Welcome to the Home screen!',
-              style: TextStyle(fontSize: 16, color: Colors.grey[600]),
+    return FlutterMap(
+      options: MapOptions(
+        initialCenter: const LatLng(20.5937, 78.9629), // Center of India, customize as needed
+        initialZoom: 5.0,
+        minZoom: 3.0,
+        maxZoom: 18.0,
+        interactionOptions: const InteractionOptions(
+          flags: InteractiveFlag.all,
+        ),
+      ),
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.akshaya.hub',
+          maxZoom: 19,
+          tileBuilder: (context, widget, tile) {
+            return ColorFiltered(
+              colorFilter: const ColorFilter.mode(
+                Colors.transparent,
+                BlendMode.multiply,
+              ),
+              child: widget,
+            );
+          },
+        ),
+        RichAttributionWidget(
+          attributions: [
+            TextSourceAttribution(
+              'OpenStreetMap contributors',
+              onTap: () {},
             ),
           ],
         ),
-      ),
+      ],
     );
   }
 
